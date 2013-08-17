534872d0a81f217924c7908b957ee7c70ab1b3b9
Richard Harrington <harrington.richard@gmail.com>
2013-08-16 23:34:30 -0400

    changed 'make-it-so' function in core so it takes fast-forward and fps parameters

--- src/robotwar/core.clj
+++ src/robotwar/core.clj
@@ -17,10 +17,10 @@
   (world/init-world 256.0 256.0 (progs))) 
 (defn combined-worlds [] 
   (world/build-combined-worlds (world)))
-(defn sim-worlds [] 
-  (animate/build-sim-worlds (combined-worlds) 25))
-(defn make-it-so []
-  (animate/animate (sim-worlds) 25 25 20.0))
+(defn sim-worlds [fast-forward] 
+  (animate/build-sim-worlds (combined-worlds) fast-forward))
+(defn make-it-so [fast-forward fps]
+  (animate/animate (sim-worlds fast-forward) 25 25 fps))
 
 (def rr register/read-register)
 (def wr register/write-register)
