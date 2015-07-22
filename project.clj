19dc97f464f703f6cddb9e8bf5f5a34981a999e2
Richard Harrington <harrington.richard@gmail.com>
2015-07-22 17:56:32 -0400

    removed javac-version from project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :javac-target "1.7"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.5.1"]
                  [ring/ring-json "0.3.1"]
