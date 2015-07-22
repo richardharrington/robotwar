1a5bb2bf03cd10f8d101f009be92979cf405e271
Richard Harrington <harrington.richard@gmail.com>
2015-07-22 14:39:48 -0400

    added min-lein-version

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.match "0.2.0-rc5"]
                  [clj-time "0.5.1"]
