c56a8f34d02eb7bb3c560b3e97147370d16c2523
Richard Harrington <harrington.richard@gmail.com>
2026-05-24 23:07:44 -0400

    Remove obsolete Ring dev server entrypoint

--- src/main/robotwar/dev_server.clj
+++
@@ -1,6 +0,0 @@
-(ns robotwar.dev-server
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [robotwar.handler :as handler]))
-
-(defn -main [& _]
-  (run-jetty #'handler/app {:port 3000 :join? true}))
